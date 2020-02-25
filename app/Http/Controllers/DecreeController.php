8a7a92bfb406890747fd435ca691c5b507942522
Juan Manuel <juan07123@gmail.com>
2020-02-25 18:03:33 -0300

    Migrations

---
+++ app/Http/Controllers/DecreeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\decree;
+use Illuminate\Http\Request;
+
+class DecreeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\decree  $decree
+     * @return \Illuminate\Http\Response
+     */
+    public function show(decree $decree)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\decree  $decree
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(decree $decree)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\decree  $decree
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, decree $decree)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\decree  $decree
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(decree $decree)
+    {
+        //
+    }
+}
